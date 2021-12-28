acf9badbeb7a78de73c4ef5ccee4eb93c38c12fa
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2021-12-28 11:40:29 +0900

    view_item_listを記録してみるテスト

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,23 +2,27 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+  private val itemStore = List(
+    Item(1, "プリンター", 10000),
+    Item(2, "パソコン", 50000)
+  )
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+  def items = Action { implicit request =>
+    Ok(views.html.items(itemStore))
+  }
 }
+
+case class Item(
+    id: Long,
+    name: String,
+    value: BigDecimal
+)
